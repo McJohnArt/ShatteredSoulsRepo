83ba589f98fff7614f04739ad10cbe6e007605d0
Andrew Engesser <andrew_engesser@hotmail.com>
2022-01-29 17:52:02 -0600

    Moved souls behind stain glass and added player turn lighting

--- Assets/Scripts/PlayersSouls.cs
+++ Assets/Scripts/PlayersSouls.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class PlayersSouls : MonoBehaviour
 {
@@ -8,18 +9,46 @@ public class PlayersSouls : MonoBehaviour
     public int GroupID;
     public bool IsConnected;
     public bool HasMadeGroup;
+    public Light2D SoulsLight;
+    private bool isMyPlayersTurn;
+    private Coroutine toggleSoulsLightCoroutine;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isMyPlayersTurn = false;
+        if (PlayerID != 1) // turn all lights except player 1's off
+            SoulsLight.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // toggle soul light on
+        if (LevelController.s.CurrentPlayersTurn + 1 == PlayerID &&
+            isMyPlayersTurn == false)
+        {
+            if (toggleSoulsLightCoroutine != null)
+                StopCoroutine(toggleSoulsLightCoroutine);
+            toggleSoulsLightCoroutine = StartCoroutine(ToggleSoulsLight(true, 0));
+            isMyPlayersTurn = true;
+        } // toggle soul light off
+        else if (LevelController.s.CurrentPlayersTurn + 1 != PlayerID &&
+            isMyPlayersTurn == true)
+        {
+            if (toggleSoulsLightCoroutine != null)
+                StopCoroutine(toggleSoulsLightCoroutine);
+            toggleSoulsLightCoroutine = StartCoroutine(ToggleSoulsLight(false, 10));
+            isMyPlayersTurn = false;
+        }
     }
+
+    public IEnumerator ToggleSoulsLight(bool value, int secToWait)
+    {
+        yield return new WaitForSeconds(10);
+        SoulsLight.gameObject.SetActive(value);
+    }
+
     public void MakeGroupID()
     {
         //if I am not a part of a group
