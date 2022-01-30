9128c43d98de5a23f3f614eb7c8a28073470d75a
Andrew Engesser <andrew_engesser@hotmail.com>
2022-01-30 12:41:35 -0600

    Added PlayerCard Round Winner Image

--- Assets/Scripts/PlayerCardInfo.cs
+++ Assets/Scripts/PlayerCardInfo.cs
@@ -12,16 +12,28 @@ public class PlayerCardInfo : MonoBehaviour
     public Slider PlayerScore;
     public Animator CardAnimator;
     public TMP_Text ClicksLeft;
+    public TMP_Text PlayerWinCounter;
+    public Image PlayerWinImage;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerWinImage.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void IncrementPlayerWinCounter()
+    {
+        int value = int.Parse(PlayerWinCounter.text);
+        if (value == 0)
+            PlayerWinImage.gameObject.SetActive(true);
+        value++;
+        PlayerWinCounter.text = value.ToString();
     }
 }
